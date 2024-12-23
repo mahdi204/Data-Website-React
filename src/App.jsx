e8a8cd5c066697c9f783f8708df6aaebded0dc0f
Mahdi <133682620+mahdi204@users.noreply.github.com>
2024-12-23 21:07:38 +0200

    newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Analytics from "./Components/Analytics";
+import Newletter from "./Components/Newletter";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newletter />
     </div>
   );
 }
